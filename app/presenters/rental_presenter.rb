01538f09f9c6c9c09420590614ee0f301e8d3a02
HenriqueMorato <riquemorato@gmail.com>
2023-02-28 20:14:54 -0300

    Presenter com SimpleDelegator

--- app/presenters/rental_presenter.rb
+++ app/presenters/rental_presenter.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-class RentalPresenter
+class RentalPresenter < SimpleDelegator
   delegate :content_tag, :t, to: :helpers
 
-  def initialize(rental)
-    @rental = rental
-  end
-
   def status
-    color_class = STATUS_COLORS.fetch(rental.status&.to_sym, 'primary')
-    content_tag :span, t(rental.status, default: 'Status não existe'), class: "badge bg-#{color_class}"
+    color_class = STATUS_COLORS.fetch(super&.to_sym, 'primary')
+    content_tag :span, t(super, default: 'Status não existe'), class: "badge bg-#{color_class}"
   end
 
   private
@@ -24,13 +20,5 @@ class RentalPresenter
 
   def helpers
     ApplicationController.helpers
-  end
-
-  def method_missing(event, *args, &block)
-    rental.public_send(event, *args, &block)
-  end
-
-  def respond_to_missing?(event, include_private = false)
-    rental.respond_to?(event, include_private)
   end
 end
